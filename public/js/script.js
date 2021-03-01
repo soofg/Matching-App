e89950b95e8a4483f99939c8787d19881e56a330
soofg <70442106+soofg@users.noreply.github.com>
2021-03-01 16:56:55 +0100

    Add files via upload

---
+++ public/js/script.js
@@ -0,0 +1,27 @@
+const menuknop = document.querySelector('button'); //pen button
+const menuknopimg = document.querySelector('img'); //pen image button
+const submenuknop = document.querySelector('li'); // submenuknop merken
+const submenuknop3 = document.querySelector('li'); //submenuknop categories
+const menulijst = document.querySelector('ul'); //pen menu
+const menulijst2 = document.querySelector('ol'); //merken menu
+const menulijst3 = document.querySelector('ol'); // catergories menu
+
+function toonMenu() {
+menulijst.classList.toggle('toonMenu');
+}
+
+
+function toonMenu2() {
+menulijst2.classList.toggle('toonMenu2');
+}
+
+function toonMenu3() {
+menulijst3.classList.toggle('toonMenu3');
+}
+
+menuknop.addEventListener('click', toonMenu);
+menuknopimg.addEventListener('click', toonMenu);
+submenuknop.addEventListener('click', toonMenu2);
+submenuknop3.addEventListener('click', toonMenu3);
+
+
