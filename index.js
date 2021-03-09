8b19b2679109e500d9ada7b810b3ca10b0889d78
soofg <sofya.gerges@hva.nl>
2021-03-09 06:22:28 +0100

    css fixing

--- index.js
+++ index.js
@@ -1,143 +1,131 @@
-console.log("hii")
+console.log('hii');
 
-//imports
+// imports
 const bodyParser = require('body-parser');
 const express = require('express');
+
 const app = express();
 const dotenv = require('dotenv').config();
+
 const port = 3000;
-const MongoClient = require('mongodb').MongoClient;
+const { MongoClient } = require('mongodb');
 const mongoose = require('mongoose');
+const ejs = require('ejs');
 const Clothes = require('./models/clothes');
 
 // verbinden met de mongo database
 let db = null;
 // function connectDB
 async function connectDB() {
-    // get URI from .env file
-    const uri = process.env.DATABASECONNECT
-    // make connection to database
-    const options = { useUnifiedTopology: true };
-    const client = new MongoClient(uri, options)
-    await client.connect();
-    db = await client.db(process.env.DATABASENAME)
+  // get URI from .env file
+  const uri = process.env.DATABASECONNECT;
+  // make connection to database
+  const options = { useUnifiedTopology: true };
+  const client = new MongoClient(uri, options);
+  await client.connect();
+  db = await client.db(process.env.DATABASENAME);
 }
 
 // een 'test' om te achterhalen of de database is verbonden
 connectDB()
-    .then(() => {
-        // if succesfull connections is made, show a message
-        console.log('We have a connection to Mongo!')
-    })
-    .catch(error => {
-        // if connnection is unsuccesful, show errors
-        console.log(error)
-    });
+  .then(() => {
+    // if succesfull connections is made, show a message
+    console.log('We have a connection to Mongo!');
+  })
+  .catch((error) => {
+    // if connnection is unsuccesful, show errors
+    console.log(error);
+  });
 
+// verbind mongoose met de database
+mongoose
+  .connect(process.env.DATABASECONNECT, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  }) // voor de errors in terminal (deprication warnings)
+  .then((result) => {
+    console.log('We have a connection to db!');
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
-mongoose.connect((process.env.DATABASECONNECT),  { useUnifiedTopology: true, useNewUrlParser: true}) //voor de errors in terminal (deprication warnings)
- .then((result) => { console.log('We have a connection to db!')
-})
-.catch((error )=> { console.log(error)
-})
-        
-
-
-
-// dit is waar de applicatie zich bevindt, hij 'luistert op port 3000, daar wordt de app weergegeven.
-//Listen on port
+// dit is waar de applicatie zich bevindt, hij 'luistert op port 3000, daar wordt de app weergegeven
+// Listen on port
 app.listen(3000, () => {
-    console.log('Express web app on localhost:3000');
+  console.log('Express web app on localhost:3000');
 });
 
 // Static files
 app.use(express.static('public'));
 app.use(express.static('public/images'));
 app.use(express.static('public/js'));
-app.use('/css', express.static(__dirname + 'public/css')); //link naar je css folder
-app.use('/js', express.static(__dirname + 'public/js')); //link naar je js folder
-app.use('/images', express.static(__dirname + 'public/images')); //link naar je images folder
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use('/css', express.static(`${__dirname}public/css`)); // link naar je css folder
+app.use('/js', express.static(`${__dirname}public/js`)); // link naar je js folder
+app.use('/images', express.static(`${__dirname}public/images`)); // link naar je images folder
+app.use(bodyParser.urlencoded({ extended: false }));
 
-
-//(EJS)Display je html op je localhost (omdat de index in de root van ons document zit, hoef je het niet te specificeren in de app.get('',))
+// (EJS)Display je html op je localhost (omdat de index in de root van ons document zit,
+// hoef je het niet te specificeren in de app.get('',))
 app.get('', (req, res) => {
-    res.render('index', { text: 'Find Friends' })
+  res.render('index', { text: 'Find Friends' });
 });
 app.get('', (req, res) => {
-    res.render('index', { text: 'Help finding outfits for occassions' })
+  res.render('index', { text: 'Help finding outfits for occassions' });
 });
 
 app.get('/menu', (req, res) => {
-    res.render('menu', { text: ' ' })
-})
+  res.render('menu', { text: ' ' });
+});
 
-//--
-// async: 
-    // let: 
-    //await: Start pas als alles is geladen 
-    // db: database > collection: submapje in je database die in dit geval informatie heeft over kleding
-    //find: zoeken (in dit geval in de database)
-    // ingevulde sort ziet er uit als {sort: {merk: -1, name: 1})} : geeft de volgorde van wat onder moet en wat bovenaan
-    //toArray: een array ervan maken, in de database hebben we het gezet met [] > zie maketheoutfit.json
 app.get('/maketheoutfit', async (req, res) => {
-    let maketheoutfit = {} // data vanuit de database
-    maketheoutfit = await db.collection('clothes').find({},{sort: {}}).toArray();
-    res.render('maketheoutfit', { text: 'What To Wear', maketheoutfit })
+  let maketheoutfit = {}; // data vanuit de database
+  maketheoutfit = await db.collection('clothes').find({}, { sort: {} }).toArray();
+  res.render('maketheoutfit', { text: 'What To Wear', maketheoutfit });
 });
 
-app.post('/maketheoutfit/:ClothedId', async (req, res) => {
-    clothes = await db.collection('clothes').find({}).toArray();
-    console.log(req.body.merken);
-    console.log(req.body.categories);
+app.post('/maketheoutfit', async (req, res) => {
+  clothes = await db.collection('clothes').find({}).toArray();
+  console.log(req.body.merken);
+  console.log(req.body.categories);
 
-    let merken = {}
-    let categories = {}
-    maketheoutfit = clothes.find(clothes => { return clothes.id === req.body.merken})
-    maketheoutfit = clothes.find(clothes => { return clothes.id === req.body.categories})
-    res.render('maketheoutfitresults', {});
+  const merken = {};
+  const categories = {};
+  maketheoutfit = clothes.find((clothes) => clothes.id === req.body.merken);
+  maketheoutfit = clothes.find((clothes) => clothes.id === req.body.categories);
+  res.render('maketheoutfitresults', {});
 });
 
-
-
-app.get('/maketheoutfit/:clothesId', async (req, res) => { 
-    const clothes = await db.collection('clothes').findOne({ id: req.params.clothesId });
-    res.render('clothingdetails', { title: 'Clothing Details', clothes });
+app.get('/maketheoutfit/:clothesId', async (req, res) => {
+  const clothes = await db
+    .collection('clothes')
+    .findOne({ id: req.params.clothesId });
+  res.render('clothingdetails', { title: 'Clothing Details', clothes });
 });
-
 
 app.get('/outfithelprequest', (req, res) => {
-    res.render('outfithelprequest', { title: "Hi" });
+  res.render('outfithelprequest', { title: 'Hi' });
 });
 
 app.get('/maketheoutfit/chosenclothing', (req, res) => {
-    console.log("laden gelukt");
-    res.render('chosenclothing', { title: "Hi" });
+  console.log('laden gelukt');
+  res.render('chosenclothing', { title: 'Hi' });
 });
-
 
 app.post('/outfithelprequest', (req, res) => {
-    res.render('sendOutfitrequest', { title: "Request has been sent!" });
+  res.render('sendOutfitrequest', { title: 'Request has been sent!' });
 });
 
-
 // Set Views
-app.set('views', './views')
-app.set('view engine', 'ejs')
-
+app.set('views', './views');
+app.set('view engine', 'ejs');
 
 // routing
 
-
-app.use(function (req, res, next) {
-    res.status(404).send("404 Not Found")
+app.use((req, res, next) => {
+  res.status(404).send('404 Not Found');
 });
 
 //
 
 app.set('view engine', 'ejs');
-let ejs = require('ejs');
-
-
-
-
--- public/js/script.js
+++ public/js/script.js
@@ -1,30 +1,24 @@
-const menuknop = document.querySelector('button'); //pen button
-const menuknopimg = document.querySelector('img'); //pen image button
+const menuknop = document.querySelector('button'); // pen button
+const menuknopimg = document.querySelector('img'); // pen image button
 const submenuknop = document.querySelector('li'); // submenuknop merken
-const submenuknop3 = document.querySelector('#submenuknop3'); //submenuknop categories
-const menulijst = document.querySelector('ul'); //pen menu
-const menulijst2 = document.querySelector('ol'); //merken menu
+const submenuknop3 = document.querySelector('#submenuknop3'); // submenuknop categories
+const menulijst = document.querySelector('ul'); // pen menu
+const menulijst2 = document.querySelector('ol'); // merken menu
 const menulijst3 = document.querySelector('#menulijst3'); // catergories menu
 
-
 function toonMenu() {
-menulijst.classList.toggle('toonMenu');
+  menulijst.classList.toggle('toonMenu');
 }
 
-
 function toonMenu2() {
-menulijst2.classList.toggle('toonMenu2');
+  menulijst2.classList.toggle('toonMenu2');
 }
 
 function toonMenu3() {
-menulijst3.classList.toggle('toonMenu3');
+  menulijst3.classList.toggle('toonMenu3');
 }
-
-
-
 
 menuknop.addEventListener('click', toonMenu);
 menuknopimg.addEventListener('click', toonMenu);
 submenuknop.addEventListener('click', toonMenu2);
 submenuknop3.addEventListener('click', toonMenu3);
-
